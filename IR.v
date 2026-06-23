8d37d3895340ec9011b24e532ea098bb92f5956a
Zeyad Elrayes <109563178+Ziad2109@users.noreply.github.com>
2026-06-23 14:23:09 +0300

    Create IR.v

---
+++ IR.v
@@ -0,0 +1,29 @@
+module IR (
+    input  wire        clk, 
+    input  wire        reset,
+    input  wire        load,
+    input  wire [7:0]  inst,
+ 
+    output reg  [2:0]  CU,    
+    output reg  [2:0]  ALU,   
+    output reg  [3:0]  MUX    
+);
+
+    always @(posedge clk or posedge reset)
+    begin 
+        if (reset)         
+        begin 
+            CU  <= 3'b000;
+            ALU <= 3'b000;
+            MUX <= 4'b0000;
+        end
+        else if (load)     
+        begin 
+            CU  <= inst[7:5];
+            ALU <= inst[6:4];
+            MUX <= inst[3:0];
+        end  
+        
+    end 
+
+endmodule
