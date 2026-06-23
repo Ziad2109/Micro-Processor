92b56cf2bc76888b8845a6a2d808e6a663495031
iahmady <i.ahmady@aucegypt.edu>
2026-06-24 02:14:05 +0300

    Fixed syntax errors and adjusted register logic

--- PC.v
+++ PC.v
@@ -3,15 +3,15 @@ input wire 		[3:0] i_address, //Unused at the moment
 input wire 		i_clk,
 input wire 		i_rst_n,
 input wire      i_jump, //Unused 
-output wire 	o_address
+output wire 	[7:0] o_address
 );
 
+reg [7:0] temp;
 always @(posedge i_clk or negedge i_rst_n)
-
 begin 
         if (!i_rst_n)
             begin 
-                o_address <=  0;
+                temp <=  0;
             end
         else if(i_jump)
             begin 
@@ -19,6 +19,10 @@ begin
             end 
         else 
             begin 
-                o_address <= o_address + 1;
+                temp <= temp + 1;
             end
 end
+assign o_address = temp; 
+
+
+endmodule 
