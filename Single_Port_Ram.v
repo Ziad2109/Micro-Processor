8017c46ae593962cf8b3cdb48ad93f0623a88a6f
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-25 00:23:37 +0300

    Refactor Single_Port_Ram module to Single_Port_RAM

--- Single_Port_Ram.v
+++ Single_Port_Ram.v
@@ -1,31 +1,23 @@
-module Single_Port_Ram #
-(
-    parameter WIDTH = 4,
-    parameter DEPTH = 16,
-    parameter AW    = $clog2(DEPTH)
-    
-)
-
-(
-   input wire clk_in,
-   input wire Read_Or_write,
-   input wire [AW - 1: 0] addr,
-   input [WIDTH - 1: 0] Data_In,
-   output wire [WIDTH - 1:0] Data_Out   
+module Single_Port_RAM #(
+    parameter width = 3,
+    parameter depth = 8,
+    parameter AW    = $clog2(depth)
+)(
+    input wire clk,
+    input wire Read_Or_Write,
+    input wire [AW - 1: 0] addr,
+    input wire [width - 1: 0] data_in,     
+    output wire [width - 1:0] data_out 
 );
 
-reg [WIDTH-1:0] mem [0:DEPTH-1];
+    reg [width-1:0] mem [0:depth-1];
 
+    always @(posedge clk) begin             
+        if (!Read_Or_Write) begin
+            mem[addr] <= data_in;           
+        end
+    end 
 
-always @(posedge clk_in) 
-begin
-    if (!Read_Or_write) begin
-
-        mem[addr] <= Data_In;
-
-    end
-end 
-
-assign Data_Out = mem[addr];
+    assign data_out = mem[addr];
 
 endmodule
