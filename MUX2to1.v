3064fb5c56f3821568ffb38e73afbb24f59fa0f4
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-25 00:25:03 +0300

    Update MUX2to1.v

--- MUX2to1.v
+++ MUX2to1.v
@@ -1,9 +1,9 @@
 module MUX2to1 (
-    input [3:0] A,    
-    input [3:0] B,    
-    input sel,
-    output [3:0] Y     
+    input wire [3:0] data_a,    
+    input wire [3:0] data_b,    
+    input wire sel,
+    output wire [3:0] data_out 
 );
-    assign Y = sel ? B : A;
+    assign data_out = sel ? data_b : data_a;
 
 endmodule
