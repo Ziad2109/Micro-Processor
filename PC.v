eb19c7c9f7c5f8cea0895b6c6ae4fc79742eef5c
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-25 00:24:41 +0300

    Refactor PC module for improved clarity and naming

--- PC.v
+++ PC.v
@@ -1,28 +1,23 @@
-module PC(
-input wire 		[3:0] i_address, //Unused at the moment
-input wire 		i_clk,
-input wire 		i_rst_n,
-input wire      i_jump, //Unused 
-output wire 	[7:0] o_address
+module PC (
+    input wire [3:0] i_address,       
+    input wire clk,
+    input wire rst_n,
+    input wire i_jump,               
+    output wire [7:0] o_address
 );
 
-reg [7:0] temp;
-always @(posedge i_clk or negedge i_rst_n)
-begin 
-        if (!i_rst_n)
-            begin 
-                temp <=  0;
-            end
-        else if(i_jump)
-            begin 
-                //This is usually not inisde the PC which should already recieve the jumped address 
-            end 
-        else 
-            begin 
-                temp <= temp + 1; // A PC usually does not do this on its own it is often connected to an adder that does this
-            end
-end
-assign o_address = temp; 
+    reg [7:0] temp;
 
+    always @(posedge clk or negedge rst_n) begin 
+        if (!rst_n) begin 
+            temp <= 0;
+        end else if (i_jump) begin 
+         
+        end else begin 
+            temp <= temp + 1;       
+        end
+    end
 
-endmodule 
+    assign o_address = temp; 
+
+endmodule
