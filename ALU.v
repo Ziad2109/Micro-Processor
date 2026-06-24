0bbb876badefb38f4ab6b739d22afbf54c6fdb98
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-25 00:27:25 +0300

    Refactor ALU module with parameterized inputs and outputs

--- ALU.v
+++ ALU.v
@@ -1,23 +1,30 @@
-module ALU(
-  input [3:0] data_mem,
-  input [3:0] acc,
-  input [2:0] opcode,
-  output reg [3:0] results,
-  output Z,
-  output C
+module ALU #(
+    parameter width = 3,
+    parameter depth = 8
+)(
+    input wire [depth - 1:0] data_in,
+    input wire [depth - 1:0] data_acc,
+    input wire [width - 1:0] opcode,
+    output reg [depth - 1:0] data_out,
+    output wire z_flag,
+    output reg c_flag
 );
-  reg carry;
-  always @(*)
-    begin
-      carry = 0;
-      case (opcode)
-        3'b000: {carry,results} = data_mem+acc;
-        3'b001: {carry,results} = data_mem-acc;
-        3'b010: results = data_mem | acc;
-        3'b011: results = data_mem & acc;
-        default: results = acc;
-      endcase
+
+    always @(*) begin
+        c_flag = 1'b0;
+        case (opcode)
+            3'b000 : {c_flag, data_out} = data_in + data_acc;
+            3'b001 : {c_flag, data_out} = data_in - data_acc;
+            3'b010 : data_out = data_in | data_acc;
+            3'b011 : data_out = data_in & data_acc;
+            3'b100 : data_out = data_in ^ data_acc;
+            3'b101 : data_out = ~data_in;
+            3'b110 : data_out = data_in << 1;
+            3'b111 : data_out = data_in >> 1;
+            default: data_out = data_in;
+        endcase
     end
-  assign C = carry;
-  assign Z = (results == 4'b0000);
+
+    assign z_flag = (data_out == 0);
+
 endmodule
