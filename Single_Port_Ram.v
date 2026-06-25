8d157696351a10d222d951a043b5e82897ee7b0f
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-25 12:44:21 +0300

    Update Single_Port_Ram.v

--- Single_Port_Ram.v
+++ Single_Port_Ram.v
@@ -4,20 +4,23 @@ module Single_Port_RAM #(
     parameter AW    = $clog2(depth)
 )(
     input wire clk,
-    input wire Read_Or_Write,
+    input wire Read,
+    input wire Write,
     input wire [AW - 1: 0] addr,
     input wire [width - 1: 0] data_in,     
-    output wire [width - 1:0] data_out 
+    output wire [depth - 1:0] data_out 
 );
 
     reg [width-1:0] mem [0:depth-1];
 
     always @(posedge clk) begin             
-        if (Read_Or_Write) begin
+        if (Write) begin
             mem[addr] <= data_in;           
         end
-		end
-		
-  assign data_out = mem[addr];
+        if (Read) begin
+            data_out <= mem[addr];
+        end
+    end 
+
 
 endmodule
