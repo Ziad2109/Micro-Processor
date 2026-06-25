d7ad8dcf84e0ad0003eca7c52e03b68d5127158b
baselezz-del <baselezz@aucegypt.edu>
2026-06-25 11:12:12 +0300

    Update IR.v

--- IR.v
+++ IR.v
@@ -5,19 +5,16 @@ module IR (
     input wire [7:0] inst,
  
     output reg [2:0] CU,
-    output reg [2:0] ALU,
     output reg [3:0] MUX
 );
 
     always @(posedge clk or negedge rst_n) begin 
         if (!rst_n) begin 
             CU  <= 3'b000;
-            ALU <= 3'b000;
             MUX <= 4'b0000;
         end else if (load) begin    // Changed to 'else if' to prevent race conditions
             CU  <= inst[7:5];
-            ALU <= inst[6:4];
-            MUX <= inst[3:0];
+            MUX <= inst[4:0];
         end  
     end 
 endmodule
