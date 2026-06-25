7ea5b89ae5ca7cbdca226a899cffd9a8a30c97b9
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-25 12:36:43 +0300

    Refactor Single_Port_RAM with new parameters and signals
    Updated RAM parameters and renamed Write signal to Read_Or_Write. Changed data_out assignment to continuous assignment.

--- Single_Port_Ram.v
+++ Single_Port_Ram.v
@@ -1,26 +1,23 @@
 module Single_Port_RAM #(
-    parameter width = 3,
-    parameter depth = 8,
+    parameter width = 8,
+    parameter depth = 256,
     parameter AW    = $clog2(depth)
 )(
     input wire clk,
-    input wire Read,
-    input wire Write,
+    input wire Read_Or_Write,
     input wire [AW - 1: 0] addr,
     input wire [width - 1: 0] data_in,     
-    output wire [depth - 1:0] data_out 
+    output wire [width - 1:0] data_out 
 );
 
     reg [width-1:0] mem [0:depth-1];
 
     always @(posedge clk) begin             
-        if (Write) begin
+        if (Read_Or_Write) begin
             mem[addr] <= data_in;           
         end
-        if (Read) begin
-            data_out <= mem[addr];
-        end
-    end 
-
+		end
+		
+  assign data_out = mem[addr];
 
 endmodule
