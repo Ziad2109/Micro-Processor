892ddb34878da6f1d054af1e02725a850bc6f158
baselezz-del <baselezz@aucegypt.edu>
2026-06-25 11:26:04 +0300

    Update ACC.v

--- ACC.v
+++ ACC.v
@@ -1,12 +1,16 @@
 module ACC(
   input clk,
   input rst_n,
+  input wire we,        
   input [3:0] D,
   output reg [3:0] Q
 );
   always @(posedge clk or negedge rst_n)
     begin
-      if (!rst_n) Q<= 4'b0000;
-      else Q <= D;
+      if (!rst_n) 
+        Q <= 4'b0000;
+      else if (we) 
+        Q <= D;
+      
     end
 endmodule
