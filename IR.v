532e6f72da8bdcf8f156fc5971ba36101f862cb8
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-28 11:29:13 +0300

    Update IR.v

--- IR.v
+++ IR.v
@@ -1,11 +1,11 @@
 module IR (
-    input wire clk, 
-    input wire rst_n,
-    input wire load,
-    input wire [7:0] inst,
+    input wire clk, // Master clock signal
+    input wire rst_n, // Active low reset signal
+    input wire load, // Instruction load enable (for fetching the instructions)
+    input wire [7:0] inst, // 8 bit instruction fetched from the program memory
  
-    output reg [2:0] CU,
-    output reg [3:0] MUX
+    output reg [2:0] CU, // 3 bit opcode sent to the control unit
+    output reg [3:0] MUX // 4 bit operand/address sent to the multiplexer.
 );
 
     always @(posedge clk or negedge rst_n) begin 
@@ -13,8 +13,8 @@ module IR (
             CU  <= 3'b000;
             MUX <= 4'b0000;
         end else if (load) begin    // Changed to 'else if' to prevent race conditions
-            CU  <= inst[7:5];
-            MUX <= inst[3:0];
+            CU  <= inst[7:5]; // Top 3 bits determine the operation type
+            MUX <= inst[3:0]; // Bottom 4 bits point to a memory address
         end  
     end 
 endmodule
