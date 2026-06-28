f0b5edf452bcc508d5c5aae0c42c5f03aa2e91c9
omar-hany7 <omar_hany@aucegypt.edu>
2026-06-28 11:22:19 +0300

    Enhance ACC.v with signal descriptions
    Added comments to clarify input and output signals.

--- ACC.v
+++ ACC.v
@@ -1,18 +1,16 @@
 module ACC(
-  input clk,
-  input rst_n,
-  input wire we,        
-  input [3:0] D,
-  output reg [3:0] Q
+  input clk, // Master clock signal
+  input rst_n, // Active low asynchronous signal
+  input wire we, // Write/enable control signal, active high
+  input [3:0] D, // 4 bit data input, coming from the data out of ALU
+  output reg [3:0] Q // Sent to the ALU, known as data_acc in the ALU code
 );
   always @(posedge clk or negedge rst_n)
     begin
       if (!rst_n) 
         Q <= 4'b0000;
       else if (we) 
-        Q <= D;
+        Q <= D; // Load new data D into the register if we is active high.
       
     end
 endmodule
-
-//this is a comment from basel
