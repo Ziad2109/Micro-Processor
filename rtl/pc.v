a587782258b7ef12fbc30d26bbb6bfb73df58a5f
Zeyad Elrayes <109563178+Ziad2109@users.noreply.github.com>
2026-06-29 12:28:37 +0300

    Update pc.v

--- rtl/pc.v
+++ rtl/pc.v
@@ -4,12 +4,20 @@
 //  Owner: __________
 // ============================================================
 module pc (
-    input  wire        clk,
-    input  wire        rst,
-    input  wire        pc_load,        // jump enable (unused - tie 0)
-    input  wire [7:0]  pc_jump_addr,   // jump target (unused)
-    output reg  [7:0]  pc_q
+    input  wire       clk,
+    input  wire       rst,
+    input  wire       pc_load,
+    input  wire [7:0] pc_jump_addr,
+    output reg  [7:0] pc_q
 );
 
+    always @(posedge clk) begin
+        if (rst)
+            pc_q <= 8'h00;
+        else if (pc_load)
+            pc_q <= pc_jump_addr;
+        else
+            pc_q <= pc_q + 8'h01;
+    end
 
 endmodule
