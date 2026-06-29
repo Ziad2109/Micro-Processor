2d6275d18a03b1dc866a414398062fcf9b9d4e55
Zeyad Elrayes <109563178+Ziad2109@users.noreply.github.com>
2026-06-29 12:29:44 +0300

    Update ir.v

--- rtl/ir.v
+++ rtl/ir.v
@@ -4,12 +4,17 @@
 //  Owner: __________
 // ============================================================
 module ir (
-    input  wire        clk,
-    input  wire        rst,
-    input  wire [7:0]  instr,
-    output reg  [7:0]  ir_q
+    input  wire       clk,
+    input  wire       rst,
+    input  wire [7:0] instr,
+    output reg  [7:0] ir_q
 );
 
-
+    always @(posedge clk) begin
+        if (rst)
+            ir_q <= 8'hF0; // NOP during reset
+        else
+            ir_q <= instr;
+    end
 
 endmodule
