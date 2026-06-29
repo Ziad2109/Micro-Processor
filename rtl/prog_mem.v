4b118ea629cb5feb155f39b32eaf0a03d7f84fb3
Zeyad Elrayes <109563178+Ziad2109@users.noreply.github.com>
2026-06-29 12:29:05 +0300

    Update prog_mem.v

--- rtl/prog_mem.v
+++ rtl/prog_mem.v
@@ -4,10 +4,21 @@
 //  Owner: __________
 // ============================================================
 module prog_mem (
-    input  wire [7:0]  addr,
-    output wire [7:0]  instr
+    input  wire [7:0] addr,
+    output wire [7:0] instr
 );
 
+    reg [7:0] rom [0:255];
 
+    initial begin
+        // Unspecified locations become NOPs.
+        integer i;
+        for (i = 0; i < 256; i = i + 1)
+            rom[i] = 8'hF0;
+
+        $readmemh("program.txt", rom);
+    end
+
+    assign instr = rom[addr];
 
 endmodule
