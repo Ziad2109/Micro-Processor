e9220eadafe448eca3f8c983ef13bef6a968d822
Zeyad Elrayes <109563178+Ziad2109@users.noreply.github.com>
2026-06-29 12:32:38 +0300

    Update acc.v

--- rtl/acc.v
+++ rtl/acc.v
@@ -4,17 +4,28 @@
 //  Owner: __________
 // ============================================================
 module acc (
-    input  wire        clk,
-    input  wire        rst,
-    input  wire        acc_load,
-    input  wire [3:0]  d,              // mux2.y
-    input  wire        z_in,           // alu.z_flag
-    input  wire        c_in,           // alu.c_flag
-    output reg  [3:0]  acc_q,
-    output reg         z_q,
-    output reg         c_q
+    input  wire       clk,
+    input  wire       rst,
+    input  wire       acc_load,
+    input  wire [3:0] d,
+    input  wire       z_in,
+    input  wire       c_in,
+    output reg  [3:0] acc_q,
+    output reg        z_q,
+    output reg        c_q
 );
 
-    
+    always @(posedge clk) begin
+        if (rst) begin
+            acc_q <= 4'h0;
+            z_q   <= 1'b1;
+            c_q   <= 1'b0;
+        end
+        else if (acc_load) begin
+            acc_q <= d;
+            z_q   <= z_in;
+            c_q   <= c_in;
+        end
+    end
 
 endmodule
